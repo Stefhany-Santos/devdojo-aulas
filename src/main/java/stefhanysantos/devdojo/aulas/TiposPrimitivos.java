a4ae74d17f23da386784a1eb5aba17fe70ada725
Stefhany Santos <stefhany.contato@outlook.com>
2025-11-11 21:30:33 -0300

    🥋 feat: aula 10 - tipos primitivos pt 01: convenções de variáveis

---
+++ src/main/java/stefhanysantos/devdojo/aulas/TiposPrimitivos.java
@@ -0,0 +1,12 @@
+package stefhanysantos.devdojo.aulas;
+
+public class TiposPrimitivos {
+    // tecla de atalho: psvm
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+
+        int age = 10;
+
+        System.out.print("Idade " + age);
+    }
+}
