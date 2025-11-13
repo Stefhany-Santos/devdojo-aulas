9d1693dc3f1c90f73324cd6c06bf4527c0c104e9
Stefhany Santos <stefhany.contato@outlook.com>
2025-11-12 21:46:06 -0300

    🥋 feat: aula 15 - tipos primitivos pt 05: Exercício

---
+++ src/main/java/stefhanysantos/devdojo/aulas/TiposPrimitivosExercicio.java
@@ -0,0 +1,13 @@
+package stefhanysantos.devdojo.aulas;
+
+public class TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Maria";
+        String endereco = "Av. Joao Pessoa";
+        double salario = 2.500;
+        String dataRecebimentoSalario = "20/12/2025";
+
+        String relatorio = "Eu " + nome + " morando no endereco " + endereco + " confirmo que recebi o salário de " + salario + ", na data " + dataRecebimentoSalario;
+        System.out.println(relatorio);
+    }
+}
