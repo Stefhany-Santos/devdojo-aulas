0aec55680989904e9732ebea4e9e73395dce9a5b
Stefhany Santos <stefhany.contato@outlook.com>
2025-11-17 20:07:03 -0300

    🥋 feat: aula 35 - Arrays pt 04 : Foreach

---
+++ src/main/java/stefhanysantos/devdojo/Arrays04.java
@@ -0,0 +1,14 @@
+package stefhanysantos.devdojo;
+
+public class Arrays04 {
+    public static void main(String[] args) {
+        // formas de declarar um array
+        int[] numeros = new int [3];
+        int[] numeros2 = {1,2,3,4,5};
+        int[] numeros3 = new int[]{5,4,3,2,1};
+
+        for (int num : numeros3){
+            System.out.println(num);
+        }
+    }
+}
