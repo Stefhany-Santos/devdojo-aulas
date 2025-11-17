d1691bf128b7f9f5d5d8bed57c62b0a724848dad
Stefhany Santos <stefhany.contato@outlook.com>
2025-11-17 17:34:55 -0300

    🥋 feat: aula 27 -  Estruturas de Repetição pt 01: Laços de repetição while, do while, for

---
+++ src/main/java/stefhanysantos/devdojo/aulas/Exercicio.java
@@ -0,0 +1,44 @@
+package stefhanysantos.devdojo.aulas;
+
+import java.util.Scanner;
+
+public class Exercicio {
+
+    private static final double FAIXA1_LIMITE = 34712;
+    private static final double FAIXA2_LIMITE = 68507;
+
+    private static final double TAXA1 = 0.0970;
+    private static final double TAXA2 = 0.3735;
+    private static final double TAXA3 = 0.4950;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o salário anual: € ");
+        double salario = sc.nextDouble();
+
+        // Não fechar scanner ligado ao System.in
+        double imposto = calcularImposto(salario);
+
+        System.out.printf("Imposto a pagar: € %.2f%n", imposto);
+    }
+
+    public static double calcularImposto(double salario) {
+        double imposto = 0;
+
+        if (salario <= FAIXA1_LIMITE) {
+            imposto = salario * TAXA1;
+
+        } else if (salario <= FAIXA2_LIMITE) {
+            imposto  = FAIXA1_LIMITE * TAXA1;
+            imposto += (salario - FAIXA1_LIMITE) * TAXA2;
+
+        } else {
+            imposto  = FAIXA1_LIMITE * TAXA1;
+            imposto += (FAIXA2_LIMITE - FAIXA1_LIMITE) * TAXA2;
+            imposto += (salario - FAIXA2_LIMITE) * TAXA3;
+        }
+
+        return imposto;
+    }
+}
---
+++ src/main/java/stefhanysantos/devdojo/aulas/LacosDeRepeticao.java
@@ -0,0 +1,18 @@
+package stefhanysantos.devdojo.aulas;
+
+public class LacosDeRepeticao {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 20){
+           System.out.println(++count);
+        }
+
+        do {
+            System.out.println("dentro do do-white? "+ ++count);
+        } while (count < 20);
+
+        for (count = 0; count < 10; count++){
+            System.out.println("For "+ ++count);
+        }
+    }
+}
