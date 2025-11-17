c8a30301b4ab76b20e001bfad9ab1fb15e74cd87
Stefhany Santos <stefhany.contato@outlook.com>
2025-11-17 19:09:24 -0300

    🥋 feat: aula 32 - Arrays pt 01

---
+++ src/main/java/stefhanysantos/devdojo/aulas/Arrays01.java
@@ -0,0 +1,14 @@
+package stefhanysantos.devdojo.aulas;
+
+public class Arrays01 {
+    public static void main(String[] args) {
+        int [] idades = new int [3];
+        idades[0] = 25;
+        idades[1] = 35;
+        idades[2] = 45;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
