8c4ac53b86b06991f5b48880806d2abc36365c21
Stefhany Santos <stefhany.contato@outlook.com>
2025-11-18 19:23:20 -0300

    🥋 feat: aula 37 - arrays multidimensionais pt 02: foreach

--- src/main/java/stefhanysantos/devdojo/aulas/Arrays04.java
+++ src/main/java/stefhanysantos/devdojo/aulas/Arrays04.java
@@ -1,4 +1,4 @@
-package stefhanysantos.devdojo;
+package stefhanysantos.devdojo.aulas;
 
 public class Arrays04 {
     public static void main(String[] args) {
---
+++ src/main/java/stefhanysantos/devdojo/aulas/ArraysMultidimensionais01.java
@@ -0,0 +1,19 @@
+package stefhanysantos.devdojo.aulas;
+
+public class ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        // Primeiro Array: Representa os meses do ano
+        // Segundo Array: Representa os dias
+
+        int[][] dias = new int[12][];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 31;
+        dias[1][1] = 28;
+        dias[1][2] = 31;
+
+        System.out.println(dias[0][0]);
+    }
+}
